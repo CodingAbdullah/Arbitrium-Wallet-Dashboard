6ef9bd8a98c42c51b803e2e3db6df8372fcf726b
Abdul <abdullahmd326@gmail.com>
2023-02-25 17:30:41 -0500

    Adding navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/navbar';
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
+      <Router>
+        <Routes>
+        </Routes>
+      </Router>
       
     </div>
   );
