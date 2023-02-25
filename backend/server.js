f7f39696b58b58325d874bc5cf3da0944b37ed3b
Abdul <abdullahmd326@gmail.com>
2023-02-25 00:58:22 -0500

    Adding frontend & backend development setup

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+
+// Spinning up a node server
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Listening to PORT " + process.env.PORT || 8080);
+})
