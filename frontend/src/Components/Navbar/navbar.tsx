2a7d59435998225e8cd2d9ee0361f05fc7ef0748
Abdul <abdullahmd326@gmail.com>
2023-02-25 19:22:00 -0500

    Adding Sidebar and Footer, styles to be completed later

--- frontend/src/Components/Navbar/navbar.tsx
+++ frontend/src/Components/Navbar/navbar.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-light bg-dark">
             <div className="container-fluid">
-                <a style={{ color: 'white' }} className="navbar-brand"><b>Arbitrium Wallet Dashboard</b></a>
+                <a style={{ color: 'white' }} href="/" className="navbar-brand"><b>Arbitrium Wallet Dashboard</b></a>
                 <form className="d-flex">
                     <input className="form-control me-2" type="search" placeholder="Enter Address" aria-label="Search" />
                     <button className="btn btn-outline-success" type="submit">Submit</button>
