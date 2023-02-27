819399fb047be3cbd773441e2b7990321948430f
Abdul <abdullahmd326@gmail.com>
2023-02-26 22:23:22 -0500

    Adding homepage main section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/navbar';
 import Sidebar from './Components/Sidebar/sidebar';
 import Footer from './Components/Footer/footer';
+import HomePage from './Components/HomePage/homepage';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Sidebar />
       <Router>
         <Routes>
+          <Route path="/" element={<HomePage />}></Route>
         </Routes>
       </Router>
       <Footer />
