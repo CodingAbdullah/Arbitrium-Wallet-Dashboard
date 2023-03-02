b731dedfc7ec694a1d6929a03ec3d9c43a795d23
Abdul <abdullahmd326@gmail.com>
2023-03-01 23:58:16 -0500

    Completing sidebar

--- frontend/src/Components/Side/side.tsx
+++ frontend/src/Components/Side/side.tsx
@@ -1,30 +1,39 @@
 // Adding Sidebar
-import SidebarSection from "../SidebarSection/sidebarsection";
-import { Link } from 'react-router-dom';
-import { Sidebar, Menu, MenuItem, useProSidebar } from 'react-pro-sidebar';
-
-const Side = () => {
-  const { collapseSidebar } = useProSidebar();
-
-  return (
-    <div style={{ display: 'flex', height: '100%' }}>
-        <Sidebar>
-            <SidebarSection />
-            <Menu>
-                <Link to="/about"><MenuItem>About</MenuItem></Link>
-                <Link to="/arb-price-lookup"><MenuItem>Arbitrium Price Lookup</MenuItem></Link>
-                <Link to="/erc721-token-holdings"><MenuItem>ERC-721 Token Holdings</MenuItem></Link>
-                <Link to="/erc-721-token-lookup"><MenuItem>ERC-721 Token Lookup</MenuItem></Link>
-                <Link to="/gas"><MenuItem>Gas</MenuItem></Link>
-                <Link to="/home"><MenuItem>Home</MenuItem></Link>
-                <Link to="/wallet-token-analytics"><MenuItem>Wallet/Token Analytics</MenuItem></Link>
-            </Menu>
-      </Sidebar>
-      <main>
-        <button onClick={() => collapseSidebar()}>Collapse</button>
-      </main>
-    </div>
-  );
+const Sidebar = () => {
+   
+    // Add the child component, SideBarMetricsSection and keep the main Sidebar component lean
+    return (
+        <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-dark sidebar collapse">
+            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom" />
+            <div className="sidebar-sticky">
+                <table className="table table-striped table-dark">
+                    <tbody>
+                        <tr>
+                            <td><a style={{color: 'white'}} href="/about">About</a></td>
+                        </tr>
+                        <tr>
+                            <td><a style={{color: 'white'}} href="/arbitrium-price-lookup">Arbitrium Price Lookup</a></td>
+                        </tr>
+                        <tr>
+                            <td><a style={{color: 'white'}} href="/erc721-token-holdings">ERC-721 Token Holdings</a></td>
+                        </tr>
+                        <tr>
+                            <td><a style={{color: 'white'}} href="/erc721-token-lookup">ERC-721 Token Lookup</a></td>
+                        </tr>
+                        <tr>
+                            <td><a style={{color: 'white'}} href="/gas">Gas</a></td>
+                        </tr>
+                        <tr>
+                            <td><a style={{color: 'white'}} href="/home">Home</a></td>
+                        </tr>
+                        <tr>
+                            <td><a style={{color: 'white'}} href="/wallet-token-analytics">Wallet/Token Analytics</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </nav>
+    );
 }
 
-export default Side;
+export default Sidebar;
