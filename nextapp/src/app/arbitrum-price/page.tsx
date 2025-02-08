490d12146a0358f874394615fb198082e8bb8b10
CodingAbdullah <abdullahmd326@gmail.com>
2025-02-08 11:11:22 -0500

    Arbitrum price section complete

---
+++ nextapp/src/app/arbitrum-price/page.tsx
@@ -0,0 +1,10 @@
+import ArbitrumPriceChart from "../components/ArbitrumPriceChart";
+
+// Arbitrum Price Page Wrapper Custom Component
+export default function ArbitrumPricePage() {
+    return (
+        <div className="bg-gray-800 text-gray-300 py-5 px-4 sm:px-6 lg:px-8 shadow-lg">
+            <ArbitrumPriceChart />
+        </div>
+    )
+}
