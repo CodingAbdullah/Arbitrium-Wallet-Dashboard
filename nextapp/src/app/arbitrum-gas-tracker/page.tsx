dad62d9ec3fc9f3fdb10c0c4adcf1e121111c872
CodingAbdullah <abdullahmd326@gmail.com>
2025-02-08 10:36:55 -0500

    Arbitrum gas metrics section complete

--- nextapp/src/app/arbitrum-gas-tracker/page.tsx
+++ nextapp/src/app/arbitrum-gas-tracker/page.tsx
@@ -1,5 +1,18 @@
+import ArbitrumGasInfoTable from "../components/ArbitrumGasInfoTable";
+
+// Arbitrum Gas Page Custom Component
 export default function ArbitrumGasPage() {
     return (
-        <div></div>
+        <div className="bg-gray-800 text-gray-300 py-10 px-4 sm:px-6 lg:px-8 shadow-lg">
+            <h1 className="text-5xl font-bold mb-6 text-center">
+                <span className="bg-clip-text text-transparent bg-gradient-to-r from-gray-400 to-gray-100">
+                    Arbitrum Gas Information
+                </span>
+            </h1>
+            <p className="text-lg mb-8 text-center">
+                <i>Lookup gas metrics related to Arbitrum</i> 
+            </p>
+            <ArbitrumGasInfoTable />
+        </div>
     )
 }
