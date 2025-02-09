431a04763a2e845b2296ffe3e71206ae25b90ee8
CodingAbdullah <abdullahmd326@gmail.com>
2025-02-08 20:20:01 -0500

    Wallet stats backend routes complete

--- nextapp/src/app/api/wallet-history-data/route.ts
+++ nextapp/src/app/api/wallet-history-data/route.ts
@@ -15,11 +15,11 @@ export async function POST(request: Request){
     }
 
     // Fetch data based on request parameters
-    const response = await fetch("https://deep-index.moralis.io/api/v2.2/wallets/" + body.walletAddress + "/profitability/summary?chain=arbitrum", options) // Pass in address values for request
+    const response = await fetch('https://deep-index.moralis.io/api/v2.2/wallets/' + body.walletAddress + '/chains?chains%5B0%5D=arbitrum', options) // Pass in address values for request
     
-    // Fetch data using the Ethereum data endpoints
+    // Fetch data using the Arbitrum data endpoints
     if (!response.ok) 
-        return NextResponse.json({ error: 'Failed to fetch Ethereum wallet statistics' }, { status: 500 });
+        return NextResponse.json({ error: 'Failed to fetch Arbitrum wallet statistics' }, { status: 500 });
     else {
         const data = await response.json();
         return NextResponse.json(data);
--- nextapp/src/app/api/wallet-stats-data/route.ts
+++ nextapp/src/app/api/wallet-stats-data/route.ts
@@ -17,9 +17,9 @@ export async function POST(request: Request){
     // Fetch data based on request parameters
     const response = await fetch('https://deep-index.moralis.io/api/v2.2/wallets/' + body.walletAddress + '/stats?chain=arbitrum', options) // Pass in address values for request
     
-    // Fetch data using the Ethereum data endpoints
+    // Fetch data using the Arbitrum data endpoints
     if (!response.ok) 
-        return NextResponse.json({ error: 'Failed to fetch Ethereum wallet statistics' }, { status: 500 });
+        return NextResponse.json({ error: 'Failed to fetch Arbitrum wallet statistics' }, { status: 500 });
     else {
         const data = await response.json();
         return NextResponse.json(data);
